0add22a73c470dff1d133d6867a1813a1b9bddf1
gionnidamico <damico.giovanni.1998@gmail.com>
2024-08-21 11:03:56 +0200

    Modified RB to use CPU and put tensors on GPU only when calling sample()

--- ReplayBuffers/replay_buffer.py
+++ ReplayBuffers/replay_buffer.py
@@ -3,10 +3,11 @@ import numpy as np
 import torch
 
 class ReplayBuffer:
-    def __init__(self, capacity):
+    def __init__(self, capacity, device):
         self.capacity = capacity
         self.buffer = []
         self.position = 0
+        self.device = device
     
     def push(self, state, action, reward, next_state, done):
         if len(self.buffer) < self.capacity:
@@ -25,8 +26,9 @@ class ReplayBuffer:
         action = torch.stack(action)  # Assuming actions are scalars or vectors
         reward = torch.stack(reward)  # Assuming rewards are scalars
         done = torch.stack(done)  # Assuming dones are scalars
-        
-        return state, action, reward, next_state, done
+
+        # Put tensors on CUDA if available only when used to avoid eating up too much gpu ram
+        return state.to(self.device), action.to(self.device), reward.to(self.device), next_state.to(self.device), done.to(self.device)
     
 
     
