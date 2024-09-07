bbb868d31c64aa99557d2dd10f4c93a92ff30d4e
RenziPaolo <paolorenzi00.1@gmail.com>
2024-09-07 13:51:15 +0200

    last model + batch tester1

---
+++ batch_tester1.bat
@@ -0,0 +1,12 @@
+
+python tester.py --sequence CO8 --save_log True --model skip_floorislava_owl_conv_ewc
+python tester.py --sequence CO8 --save_log True --model skip_hideandseek_owl_conv_ewc
+python tester.py --sequence CO8 --save_log True --model skip_chainsaw_owl_conv_ewc
+python tester.py --sequence CO8 --save_log True --model skip_raisetheroof_owl_conv_ewc
+python tester.py --sequence CO8 --save_log True --model skip_runandgun_owl_conv_ewc
+python tester.py --sequence CO8 --save_log True --model skip_healthgathering_owl_conv_ewc
+python tester.py --sequence CO8 --save_log True --model big_gamma_owl_conv_ewc
+python tester.py --sequence CO8 --save_log True --model big_rb_owl_conv_ewc
+python tester.py --sequence CO8 --save_log True --model COC_owl_conv_ewc
+python tester.py --sequence CO8 --save_log True --model repeat_scenarios_owl_conv_ewc
+python tester.py --sequence CO8 --save_log True --model vanilla_rb_owl_conv_ewc
